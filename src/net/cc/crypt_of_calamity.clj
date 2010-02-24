c43b16114246411276292b7403325e70c1767bb8
geoff.ulman <geoff.ulman@0bcb31aa-20c9-11df-a325-4db0301e4011>
2010-02-24 00:38:12 +0000

    Initial revision! Hooray! Added a default namespace net.cc.crypt-of-calamity, the default clojure libraries, and a simple (although not simple enough, the clojure compilation story seems a bit off unless--and this is definitely a possibility--I'm not understanding everything) ant build script.

---
+++ src/net/cc/crypt_of_calamity.clj
@@ -0,0 +1,19 @@
+(ns net.cc.crypt-of-calamity
+  (:import (java.awt Color Dimension)
+           (javax.swing JPanel JFrame Timer JOptionPane)
+           (java.awt.event ActionListener KeyListener))
+  (:use clojure.contrib.import-static
+        [clojure.contrib.seq-utils :only (includes?)]))
+(import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
+
+(def dirs { :up    [ 0  1]
+            :down  [ 0 -1]
+            :left  [-1  0]
+            :right [ 0 -1]})
+
+(def dir-keys { VK_UP     :up
+               VK_DOWN   :down
+               VK_LEFT   :left
+               VK_RIGHT  :right })
+
+
