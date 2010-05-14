910f9d199c5fb0e62afabaedc6e39519adb27ba8
geoff.ulman <geoff.ulman@0bcb31aa-20c9-11df-a325-4db0301e4011>
2010-05-14 01:42:25 +0000

    (crypt-of-calamity) pops up a JFrame which does nothing

--- repl.sh
+++ repl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -cp build:lib/clojure.jar:lib/clojure-contrib.jar:lib/jline-0_9_5.jar jline.ConsoleRunner clojure.main
+#java -cp build:src:lib/clojure.jar:lib/clojure-contrib.jar:lib/jline-0_9_5.jar jline.ConsoleRunner clojure.main
+java -cp src:lib/clojure.jar:lib/clojure-contrib.jar:lib/jline-0_9_5.jar jline.ConsoleRunner clojure.main
