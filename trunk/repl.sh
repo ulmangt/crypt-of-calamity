5233f5d7db5ab6ad459552b356052eb580126a69
geoff.ulman <geoff.ulman@0bcb31aa-20c9-11df-a325-4db0301e4011>
2010-10-07 21:11:58 +0000

    reorganized repository to support leiningen.

--- trunk/repl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#java -cp build:src:lib/clojure.jar:lib/clojure-contrib.jar:lib/jline-0_9_5.jar jline.ConsoleRunner clojure.main
-java -cp src:lib/clojure.jar:lib/clojure-contrib.jar:lib/jline-0_9_5.jar jline.ConsoleRunner clojure.main
---
+++ trunk/trunk/test/crypt_of_calamity/test/core.clj
@@ -0,0 +1,6 @@
+(ns crypt-of-calamity.test.core
+  (:use [crypt-of-calamity.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
